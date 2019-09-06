c0afeed0de49360cdc9dc95b50c2954093adf235
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2019-09-06 18:09:13 +0200

    Change XMLTag into class and implement XMLHandler

---
+++ lib/xml_to_json/handlers/xml_handler.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require 'rexml/sax2listener'
+
+module XMLToJson
+  module Handlers
+    class XMLHandler
+      include REXML::SAX2Listener
+      include XMLToJson::Tags
+      attr_reader :xml_hash
+
+      def initialize
+        @xml_hash = {}
+        @stack = []
+      end
+
+      def start_element(_uri, _localname, qname, attributes)
+        prefixed_attributes = attributes
+                              .transform_keys { |key| "_#{key}".to_sym }
+        element = XMLTag.new(tag: qname.to_sym, attributes: prefixed_attributes)
+        @stack.prepend(element)
+      end
+
+      def end_element(_uri, _localname, _qname)
+        if @stack.length > 1
+          child = @stack.shift
+          parent = @stack.shift
+          parent.add_to_attributes(child.tag, child.attributes)
+          @stack.prepend(parent)
+        else
+          element = @stack.shift
+          @xml_hash[element.tag] = element.attributes
+        end
+      end
+
+      def characters(text)
+        return if text.strip.empty?
+
+        element = @stack.shift
+        element.attributes[:_text] = text
+        @stack.prepend(element)
+      end
+    end
+  end
+end
--- lib/xml_to_json/tags/xml_tag.rb
+++ lib/xml_to_json/tags/xml_tag.rb
@@ -2,6 +2,24 @@
 
 module XMLToJson
   module Tags
-    XMLTag = Struct.new(:tag, :attributes, keyword_init: true)
+    class XMLTag
+      attr_reader :tag, :attributes
+
+      def initialize(tag:, attributes:)
+        @tag = tag
+        @attributes = attributes
+      end
+
+      def add_to_attributes(tag, attributes)
+        if @attributes[tag].nil?
+          @attributes[tag] = attributes
+        elsif @attributes[tag].is_a?(Array)
+          @attributes[tag] << attributes
+        else
+          same_tags = [@attributes[tag]]
+          @attributes[tag] = same_tags << attributes
+        end
+      end
+    end
   end
 end
