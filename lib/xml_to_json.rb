80e0fe61c91071703df055310a968db813ae4a3b
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2019-09-06 18:22:30 +0200

    Add Document class

--- lib/xml_to_json.rb
+++ lib/xml_to_json.rb
@@ -1,6 +1,34 @@
-require "xml_to_json/version"
+# frozen_string_literal: true
 
-module XmlToJson
-  class Error < StandardError; end
-  # Your code goes here...
+require_relative 'xml_to_json/version'
+require_relative 'xml_to_json/tags/xml_tag'
+require_relative 'xml_to_json/handlers/xml_handler'
+require 'rexml/parsers/sax2parser'
+require 'json'
+
+module XMLToJson
+  class Document
+    def initialize(xml_document)
+      @document = File.new(xml_document)
+      @to_hash = nil
+    end
+
+    def to_hash
+      @to_hash ||= parse
+    end
+
+    def to_json(*_args)
+      to_hash.to_json
+    end
+
+    private
+
+    def parse
+      parser = REXML::Parsers::SAX2Parser.new(@document)
+      handler = Handlers::XMLHandler.new
+      parser.listen(handler)
+      parser.parse
+      handler.xml_hash
+    end
+  end
 end
