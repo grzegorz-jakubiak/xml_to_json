476771057b59fe3bbb27c0fdca95937cc00af4b8
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2020-08-09 07:48:33 +0200

    Update gems

--- gems.rb
+++ gems.rb
@@ -5,6 +5,8 @@ git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 # Specify your gem's dependencies in xtj.gemspec
 gemspec
 
+gem 'rake', '~> 12.3'
+
 group :lint do
-  gem 'rubocop', '~> 0.74.0', require: false
+  gem 'rubocop', '~> 0.88.0', require: false
 end
